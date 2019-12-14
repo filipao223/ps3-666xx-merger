71d5395ae5b0c8f19aedc66f058a65b70c83ec18
filipao223 <jpmontenegro67@gmail.com>
2019-12-14 23:44:08 +0000

    Upload script.

---
+++ src/script.bat
@@ -0,0 +1,59 @@
+:: Display directory contents
+echo off
+
+setlocal EnableDelayedExpansion
+
+cls
+
+::Accesses each subdirectory and checks for files needing merge
+for /R %cd% %%e in (.) do (
+    cd %%e
+    echo.
+    echo Currently in %%e
+    echo.
+
+    if exist *.666* call :merge
+)
+
+goto END
+
+
+
+::Main subroutine
+:merge
+
+
+::Find every unique filename with a .666xx extension
+set /a "ID=1"
+for %%f in (*.66600) do (
+    set filename[!ID!]=%%~nf
+    set /a "ID+=1"
+)
+
+::Lists which split files we found
+echo.
+echo FOUND THESE SPLIT FILES
+:: Make a count of how many 'filename[n]' variables
+for /f %%a in ('set filename^|find /c /v ""') do set count=%%a
+:: Then display them using a for from 1 to 'count'
+for /l %%a in (1,1, %count%) do echo !filename[%%a]!
+
+::------------Now start merging------------
+
+for /l %%a in (1,1, %count%) do (
+    echo.
+    echo -----Merging !filename[%%a]! -------
+    copy /b !filename[%%a]!.666* "!filename[%%a]!"
+)
+
+::Unset the filename variable(s)
+for /l %%a in (1,1, %count%) do set filename[%%a]=
+
+exit /b
+
+
+
+:END
+echo.
+echo Program has ended
+pause
